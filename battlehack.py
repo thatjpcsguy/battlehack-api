889ba8d24e60f4bf55b6a9bc8705121e9e9908ee
James Cooper-Stanbury <jamescs@freelancer.com>
2014-07-27 03:24:56 +1000

    first push to server

--- battlehack.py
+++ battlehack.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask import url_for
+from flask import render_template
 import hashlib
 import base64
 import os
 import PIL
 from PIL import Image
+from werkzeug.contrib.fixers import ProxyFix
 
+import MySQLdb
+import MySQLdb.cursors
 from base64 import decodestring
 from apns import APNs, Payload
 
@@ -30,10 +35,25 @@ import flask
 app = Flask(__name__)
 app.debug = True
 
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 _CLIENT_ID = ''
 _CLIENT_SECRET = ''
 
-con = _mysql.connect(host="127.0.0.1", user="battlehack", passwd="lolol1234", db="battlehack")
+
+# DB = {
+#     'host': '127.0.0.1',
+#     'user': 'battlehack',
+#     'passwd': 'lolol1234',
+#     'db': 'battlehack',
+#     'port': 3306,
+# }
+
+db = MySQLdb.connect(host = "127.0.0.1", user = "battlehack", passwd = "lolol1234", db = "battlehack", cursorclass=MySQLdb.cursors.DictCursor)
+db_cur = db.cursor()
+
+# con = _mysql.connect(host="127.0.0.1", user="battlehack", passwd="lolol1234", db="battlehack")
 # fb = facebook.FacebookAPI(client_id=_CLIENT_ID,client_secret=_CLIENT_SECRET)
 
 # graph = facebook.GraphAPI('656163467792074|8SjMIhn45uYxGpbGkIePkNpY3zM')
@@ -59,20 +79,25 @@ def coordinate_bounds(lat, lon, dist):
 
 @app.route("/")
 def hello():
-    return jsonify(status ="Nothing to see here!")
+    return render_template('index.html')
 
 
 @app.route('/nearby/<lat>/<lon>/<dist>', methods=['GET'])
 def nearby(lat, lon, dist):
     lat_upper, lat_lower, lon_upper, lon_lower = coordinate_bounds(float(lat), float(lon), float(dist))
 
-    con.query("SELECT * FROM listings l WHERE status = 'OPEN' AND lat BETWEEN {0} AND {1} AND lon BETWEEN {2} AND {3}".format(min(lat_lower, lat_upper), max(lat_lower, lat_upper), min(lon_lower, lon_upper), max(lon_lower, lon_upper)))
-    res = con.store_result()
-    rows = res.fetch_row(how=1)
+    # con.query("SELECT * FROM listings l WHERE status = 'OPEN' AND lat BETWEEN {0} AND {1} AND lon BETWEEN {2} AND {3}".format(min(lat_lower, lat_upper), max(lat_lower, lat_upper), min(lon_lower, lon_upper), max(lon_lower, lon_upper)))
+    
+    db_cur.execute("SELECT * FROM listings l")
+    # res = con.store_result()
+    
+    rows = db_cur.fetchall()
+    # print rows
 
     out = {"data":[]}
 
     for row in rows:
+        # print row
         dist = haversine((float(lat), float(lon)), (float(row['lat']), float(row['lon'])))
         row['dist'] = dist*1000
         out["data"].append(row)
@@ -89,6 +114,7 @@ def img(path):
 @app.route('/listing', methods=['POST'])
 def listing():
     listing_info = json.loads(request.form['data'])
+    # print listing_info
 
     #clean up image data
     image_data = listing_info['image_data'].encode('ascii').replace(' ', '+')
@@ -107,24 +133,52 @@ def listing():
     fh.write(decoded_data)
     fh.close()
 
-    size = 50, 50
+    size = 60, 60
     try:
         im = Image.open("static/images/"+filename)
-        im.thumbnail(size, Image.ANTIALIAS)
-        im.save("static/images/"+thumbname, "PNG")
+
+        # print im.size
+        w, h = im.size
+        if w > h:
+            h1 = 0;
+            h2 = h;
+            w1 = math.floor((w-h)/2);
+            w2 = w1 + h;
+
+        elif h > w:
+            w1 = 0;
+            w2 = w;
+            h1 = math.floor((h-w)/2);
+            h2 = h1 + w;
+
+        else:
+            h1 = 0;
+            h2 = h;
+            w1 = 0;
+            w2 = w;
+
+        new = im.crop((int(w1), int(h1), int(w2), int(h2)))
+        new.thumbnail(size, Image.ANTIALIAS)
+
+        new.save("static/images/"+thumbname)
+
     except IOError:
         print "cannot create thumbnail for '%s'" % infile
 
         
 
-    con.query("INSERT INTO  listings (user_id, lat, lon, title, image_url, description, price, STATUS) VALUES (%s, %s, %s, %s, %s, %s, %s, 'OPEN')" % (listing_info['user_id'], listing_info['lat'], listing_info['lon'], listing_info['title'], listing_info['description'], listing_info['price']))
+    db_cur.execute("INSERT INTO  listings (user_id, lat, lon, title, image_url, thumbnail_url, description, price, status) VALUES (%s, %s, %s, '%s', '%s', '%s', '%s', %s, 'OPEN')" % (listing_info['FBUser']['id'], listing_info['lat'], listing_info['long'], listing_info['title_data'], "http://192.168.96.81:5000/img/"+filename, "http://192.168.96.81:5000/img/"+thumbname, listing_info['desc'], listing_info['price']))
+    db.commit()
+    return jsonify(status=True)
+
+
 
 @app.route('/users', methods=['POST'])
 def users():
     if request.method == 'POST':
         user_info = json.loads(request.form['data'])
-        con.query("INSERT INTO users (user_id, first_name, last_name, email) VALUES ('%s', '%s', '%s', '%s') ON DUPLICATE KEY UPDATE last_login_time = NOW()" % (user_info['id'], user_info['first_name'], user_info['last_name'], user_info['email']))
-        con.query("INSERT INTO devices (user_id, token) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE token = '%s'" % (user_info['id'], user_info['device_token'], user_info['device_token']))
+        db_cur.execute("INSERT INTO users (user_id, first_name, last_name, email) VALUES ('%s', '%s', '%s', '%s') ON DUPLICATE KEY UPDATE last_login_time = NOW()" % (user_info['id'], user_info['first_name'], user_info['last_name'], user_info['email']))
+        db_cur.execute("INSERT INTO devices (user_id, token) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE token = '%s'" % (user_info['id'], user_info['device_token'], user_info['device_token']))
         return jsonify(status=True)
     elif request.method == 'GET':
         return jsonify(status=False)
